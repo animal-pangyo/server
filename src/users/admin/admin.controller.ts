00682cbfb4b76d21aac5101d26489073477486f3
yun <ilovezltzot@naver.com>
2023-06-08 00:03:06 +0900

    추가 : admin, delete, logout

--- src/users/admin/admin.controller.ts
+++ src/users/admin/admin.controller.ts
@@ -21,17 +21,9 @@ export class UsersController {
     return this.usersService.getUserList();
   }
 
-  // @Delete('users/:user_id')
-  // @Roles(['admin'])
-  // async deleteUser(
-  //   @Param('user_id') user_id: string,
-  //   @Roles() roles: string[],
-  // ) {
-  //   if (!roles.includes('admin')) {
-  //     throw new ForbiddenException(
-  //       '접근이 거부되었습니다. 관리자만 사용자를 삭제할 수 있습니다.',
-  //     );
-  //   }
-  //   return this.usersService.deleteUser(user_id);
-  // }
+  @Delete('users/:user_id')
+  @Roles(['admin'])
+  async deleteUser(@Param('user_id') user_id: string) {
+    return this.usersService.deleteUser(user_id);
+  }
 }
