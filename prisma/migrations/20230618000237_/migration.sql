1a45c555b59c8a107e8920fc3a223ca0f064d1c9
HYO00 <hyo02222@gmail.com>
2023-06-18 09:55:56 +0900

    마이그레이션 삭제

--- prisma/migrations/20230618000237_/migration.sql
+++ prisma/migrations/20230618000237_/migration.sql
@@ -9,8 +9,12 @@ CREATE TABLE `User` (
     `updated_at` DATETIME(3) NOT NULL,
     `roles` VARCHAR(191) NOT NULL,
     `phone` VARCHAR(191) NOT NULL,
-    `address` VARCHAR(191) NOT NULL,
-    `birth` VARCHAR(191) NOT NULL,
+    `address1` VARCHAR(191) NOT NULL,
+    `address2` VARCHAR(191) NOT NULL,
+    `month` VARCHAR(191) NOT NULL,
+    `year` VARCHAR(191) NOT NULL,
+    `day` VARCHAR(191) NOT NULL,
+    `atn` TEXT NOT NULL,
 
     UNIQUE INDEX `User_user_id_key`(`user_id`),
     UNIQUE INDEX `User_email_key`(`email`),
@@ -23,7 +27,7 @@ CREATE TABLE `Post` (
     `board_id` INTEGER NOT NULL,
     `title` VARCHAR(191) NOT NULL,
     `content` VARCHAR(191) NOT NULL,
-    `author_id` INTEGER NOT NULL,
+    `author_id` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `post_pw` VARCHAR(191) NULL,
 
@@ -51,14 +55,42 @@ CREATE TABLE `Comment` (
     PRIMARY KEY (`comment_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Review` (
+    `review_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `store_id` INTEGER NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`review_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Like` (
+    `Like_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `store_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`Like_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Post` ADD CONSTRAINT `Post_board_id_fkey` FOREIGN KEY (`board_id`) REFERENCES `Board`(`board_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Post` ADD CONSTRAINT `Post_author_id_fkey` FOREIGN KEY (`author_id`) REFERENCES `User`(`idx`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Post` ADD CONSTRAINT `Post_author_id_fkey` FOREIGN KEY (`author_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Comment` ADD CONSTRAINT `Comment_post_id_fkey` FOREIGN KEY (`post_id`) REFERENCES `Post`(`post_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Comment` ADD CONSTRAINT `Comment_author_id_fkey` FOREIGN KEY (`author_id`) REFERENCES `User`(`idx`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Review` ADD CONSTRAINT `Review_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Like` ADD CONSTRAINT `Like_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`idx`) ON DELETE CASCADE ON UPDATE CASCADE;
