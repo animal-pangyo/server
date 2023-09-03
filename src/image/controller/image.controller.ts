ab20e351b423d01535bb288df5ebaade81e9c6c7
suld2495 <suld7777@gmail.com>
2023-09-03 17:04:06 +0900

    주석 추가

--- src/image/controller/image.controller.ts
+++ src/image/controller/image.controller.ts
@@ -5,15 +5,20 @@ import multer from 'multer';
 import fs from 'fs';
 import { CreateImageDto } from '../dto/update-image';
 
+/* 업로드 한 파일을 서버에 저장하기 위한 옵션 설정 */
 const storage = multer.diskStorage({
+  // 업로드할 위치 설정
   destination: function (req, file, cb) {
     const PATH = 'client/dist/uploads/';
-    if (fs.existsSync(PATH)) {
+
+    // 디렉토리가 존재하지 않는 경우 디렉토리 생성
+    if (!fs.existsSync(PATH)) {
       fs.mkdirSync(PATH, { recursive: true });
     }
-
     cb(null, PATH)
   },
+
+  // 업로드할 파일 이름 설정
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + '.' + file.originalname.split('.').at(-1))
   }
@@ -31,7 +36,7 @@ export class ImageController {
     @UploadedFile() image: Express.Multer.File,
     @Param('chatidx') chatidx: string,
     @Body() userid: CreateImageDto) {
-    await this.imageService.upload(image, chatidx, userid.userid);
+    await this.imageService.upload(image, chatidx, userid.userid); // CreateImageDto를 사용하여 이미지 업로드 요청을 ImageService로 전달
     return `/uploads/${image.filename}`;
   }
 }
--- src/image/service/image.service.ts
+++ src/image/service/image.service.ts
@@ -6,13 +6,14 @@ import { PrismaService } from 'src/prisma.service';
 export class ImageService {
   constructor(private readonly prismaService: PrismaService) { } // PrismaService를 주입받는 생성자
   async upload(image: Express.Multer.File, chatidx: string, userid: string) {
+    // 이미지 업로더를 사용하여 이미지 업로드 요청을 PrismaService로 전달
     const response = await this.prismaService.resource.create({
       data: {
         img: image.filename
       }
     });
 
-
+    // 이미지 업로드 된 idex를 사용하여 채팅 메시지 저장
     await this.prismaService.chatMsg.create({
       data: {
         img: response.idx,
